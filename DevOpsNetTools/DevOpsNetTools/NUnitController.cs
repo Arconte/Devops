df086b6e0d96dd4ffb61c185e9f0c6abb16aa653
Gregory Valderrama <gvalderrama@terremark.com>
2016-04-03 16:52:49 -0500

    ADD

--- DevOpsNetTools/DevOpsNetTools/NUnitController.cs
+++ DevOpsNetTools/DevOpsNetTools/NUnitController.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,18 +11,58 @@ namespace DevOpsNetTools
 {
     public class NUnitController
     {
-        public string Merge(IEnumerable<string> Paths)
+        private string Destination;
+        private ProcessController ProcessController = new ProcessController(); 
+        public NUnitController(string Destination)
+        {
+            this.Destination = Destination;
+        }    
+        public bool Execute(string Package, string Categories)
+        {
+            var directory =  Path.Combine(this.Destination, Package);
+            var target =  Path.Combine(directory, "RunTest.ps1");
+
+            if (!File.Exists(target))
+                return false;
+
+            if (string.IsNullOrWhiteSpace(Categories))
+            {
+                var args = string.Format(@" -ExecutionPolicy ByPass -file ""{0}"" ", target );
+                this.ProcessController.Run(args, directory );            
+            }
+            else {
+                var args = string.Format(@" -ExecutionPolicy ByPass -file ""{0}"" -Categories ""{1}""", target, Categories);
+                this.ProcessController.Run(args, directory);            
+            }
+            return true;
+        }
+
+
+        public string MergeResults(IEnumerable<string> Packages)
+        {
+            List<string> elements = new List<string>();
+            foreach (var item in Packages)
+            {
+                var directory = Path.Combine(Destination, item);
+                var target = Path.Combine(directory, "TestResult.xml");
+                if (File.Exists(target))
+                {
+                    var str = File.ReadAllText(target);
+                    elements.Add(str);  
+                }                
+            }
+            return Merge(elements); 
+        }
+        private string Merge(IEnumerable<string> Contents)
         {
             var Elements = new List<XElement>();
-
-            foreach (var item in Paths)
+            foreach (var item in Contents)
             {
                 Elements.Add(XElement.Parse(item));
             }
-
             return this.Merge(Elements).ToString(); 
         }
-        public XElement Merge(IEnumerable<XElement> Paths)
+        private  XElement Merge(IEnumerable<XElement> Paths)
         {
             //first item  mark the common attributes             
             var root = new XElement(Paths.ElementAt(0));
@@ -35,6 +77,6 @@ namespace DevOpsNetTools
                 root.Add(suite);
             }
             return root;
-        }
+        }        
     }
 }
