a8958286b4393c9bd1d823437b51542590c035b8
valderramag <gcvalderrama@hotmail.com>
2016-04-02 23:11:05 -0500

    add

---
+++ DevOpsNetTools/DevOpsNetTools.Console/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevOpsNetTools.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DevOpsNetTools.NUnitController controller = new NUnitController();
+            var str = File.ReadAllText(@"D:\Arconte\Templar\Tests\Templar.Service.SmokeTest\bin\Debug\TestResult.xml");
+            var stra = File.ReadAllText(@"D:\Arconte\Templar\Tests\Templar.Service.FunctionalTest\bin\Debug\TestResult.xml");
+            var result =  controller.Merge(new List<string>() { str, stra });
+            File.WriteAllText("output.xml", result);
+        }
+    }
+}
---
+++ DevOpsNetTools/DevOpsNetTools/NUnitController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace DevOpsNetTools
+{
+    public class NUnitController
+    {
+        public string Merge(IEnumerable<string> Paths)
+        {
+            var Elements = new List<XElement>();
+
+            foreach (var item in Paths)
+            {
+                Elements.Add(XElement.Parse(item));
+            }
+
+            return this.Merge(Elements).ToString(); 
+        }
+        public XElement Merge(IEnumerable<XElement> Paths)
+        {
+            //first item  mark the common attributes             
+            var root = new XElement(Paths.ElementAt(0));
+            var env = root.Element("environment");
+            var culture = root.Element("culture-info");
+            root.RemoveNodes();
+            root.Add(env);
+            root.Add(culture);
+            foreach (var item in Paths)
+            {
+                var suite = item.Elements("test-suite").ToList();
+                root.Add(suite);
+            }
+            return root;
+        }
+    }
+}
